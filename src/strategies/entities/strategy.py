110fe083d732a14fb54c66ddd5ea946529b479e0
geetnsh2k1 <geetansh2k1@gmail.com>
2024-04-13 13:43:45 +0530

    Uniqueness factor removed from services & strategy

--- src/strategies/entities/strategy.py
+++ src/strategies/entities/strategy.py
@@ -11,7 +11,7 @@ class Strategy(BaseModel):
     id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
 
     # content
-    heading = Column(String(255), unique=True, nullable=False)
+    heading = Column(String(255), nullable=False)
     sub_heading = Column(Text, nullable=True)
     description = Column(Text, nullable=False)
 
